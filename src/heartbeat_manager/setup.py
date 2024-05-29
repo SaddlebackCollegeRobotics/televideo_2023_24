4fc6113d6a9f4119689d4211c7fd9bdff7d1ce1d
Supernova1114 <supernovawarriors128@gmail.com>
2024-05-28 21:00:02 -0700

    clean up repo and updated heartbeat manager
    Former-commit-id: 413f02eb1d5019782755d19200441a8358ee690a

--- src/heartbeat_manager/setup.py
+++ src/heartbeat_manager/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'heartbeat_manager'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.yaml')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
